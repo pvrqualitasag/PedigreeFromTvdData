e8c8fd0460d6ab6275b4f79bf7ff47ef39c5a262
Sophie Kunz <skn@MacBook-SKN-2016.local>
2017-11-09 07:47:48 +0100

    Check Output

--- R/pedigree_output.R
+++ R/pedigree_output.R
@@ -17,10 +17,9 @@ output <- function(plPedigree, psOutfile = sOutputFile, psSepChar = "\t"){
   ### # initialize result
   for(idxPed in 1:length(plPedigree)){
     lCurrentAni <- plPedigree[[idxPed]]
-    cat(plPedigree[[lCurrentAni$TierId]]$NumId, psSepChar,
-        plPedigree[[lCurrentAni$VaterId]]$NumId, psSepChar,
-        plPedigree[[lCurrentAni$MutterId]]$NumId, psSepChar,
-        plPedigree[[lCurrentAni$TierId]]$TierId, psSepChar,
+    cat(plPedigree[[lCurrentAni$TierId]]$TierId, psSepChar,
+        plPedigree[[lCurrentAni$TierId]]$VaterId, psSepChar,
+        plPedigree[[lCurrentAni$TierId]]$MutterId, psSepChar,
         plPedigree[[lCurrentAni$TierId]]$Geburtsdatum,"\n",
         file = psOutfile,
         append = TRUE)
